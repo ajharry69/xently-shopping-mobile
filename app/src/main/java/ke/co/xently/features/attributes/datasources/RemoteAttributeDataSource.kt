4a7d70600f4bce224c355dcb3a91096e360478e2
Orinda Harrison (Mitch) <mitchke@duck.com>
2023-07-02 14:39:01 +0300

    Replace deprecated functions

--- app/src/main/java/ke/co/xently/features/attributes/datasources/RemoteAttributeDataSource.kt
+++ app/src/main/java/ke/co/xently/features/attributes/datasources/RemoteAttributeDataSource.kt
@@ -5,17 +5,12 @@ import ke.co.xently.features.attributes.models.Attribute
 import ke.co.xently.remotedatasource.SendHttpRequest
 import javax.inject.Inject
 import javax.inject.Singleton
-import kotlin.random.Random
 
 @Singleton
 class RemoteAttributeDataSource @Inject constructor(
     private val service: AttributeService,
 ) : AttributeDataSource<Attribute.RemoteRequest, Attribute.RemoteResponse> {
     override suspend fun getAttributeSearchSuggestions(query: Attribute.RemoteRequest): List<Attribute.RemoteResponse> {
-        return List(Random(0).nextInt(5)) {
-            query.toRemoteResponse()
-                .copy(name = buildString { append(query.name); append(it + 1) })
-        }
         return SendHttpRequest {
             service.searchSuggestions(query = query.name)
         }.getOrThrow()._embedded.viewModels
--- app/src/main/java/ke/co/xently/features/attributesvalues/datasources/RemoteAttributeValueDataSource.kt
+++ app/src/main/java/ke/co/xently/features/attributesvalues/datasources/RemoteAttributeValueDataSource.kt
@@ -5,17 +5,12 @@ import ke.co.xently.features.attributesvalues.models.AttributeValue
 import ke.co.xently.remotedatasource.SendHttpRequest
 import javax.inject.Inject
 import javax.inject.Singleton
-import kotlin.random.Random
 
 @Singleton
 class RemoteAttributeValueDataSource @Inject constructor(
     private val service: AttributeValueService,
 ) : AttributeValueDataSource<AttributeValue.RemoteRequest, AttributeValue.RemoteResponse> {
     override suspend fun getAttributeValueSearchSuggestions(query: AttributeValue.RemoteRequest): List<AttributeValue.RemoteResponse> {
-        return List(Random(0).nextInt(5)) {
-            query.toRemoteResponse()
-                .copy(value = buildString { append(query.value); append(it + 1) })
-        }
         return SendHttpRequest {
             service.searchSuggestions(query = query.toString())
         }.getOrThrow()._embedded.viewModels
